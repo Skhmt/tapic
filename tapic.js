aec90984feae147ede0b7febe4752f0122838da4
Skhmt <Skhmt@users.noreply.github.com>
2016-07-13 15:31:42 -0700

    Update tapic.js

--- tapic.js
+++ tapic.js
@@ -1,6 +1,6 @@
 /*
   Twitch API & Chat in javascript - TAPIC.js
-  Version 2.7.2 - 11 July 2016
+  Version 2.7.3 - 11 July 2016
   Made by skhmt - http://skhmt.github.io
 
   Compile/minify at: https://closure-compiler.appspot.com/
